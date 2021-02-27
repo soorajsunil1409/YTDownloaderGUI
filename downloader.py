824d7fcda65766e57f7c287346a236d37ab94c86
soorajsunil1409 <sooraj.namboothiry@gmail.com>
2021-02-27 14:23:06 +0530

    Revert "Revert "Revert "downloader gui done"""
    This reverts commit 7251db7b1bf1f275d0e3fc8476435780c68c3341.

--- downloader.py
+++ downloader.py
@@ -1,40 +1,12 @@
 from tkinter import *
-import pytube
 
 class Downloader(Tk):
     def __init__(self):
         super().__init__()
         self.title("Video Downloader")
-        self.geometry("600x400")
+        self.geometry("600x600")
 
-        self.link_str = StringVar()
-        self.path_str = StringVar()
-
-        self.init_widgets()
-        
         self.mainloop()
-
-    def init_widgets(self):
-        title = Label(self, text="Video Downloader", bg="red", height=2, fg="#fff", font=("Helvetica", 35, "bold"))
-        title.place(height=100, width=600, x=0, y=0)
-
-        link_lbl = Label(self, text="Enter video link: ", font=("Helvetica", 22, "bold"))
-        link_lbl.place(width=200, x=0, y=120)
-
-        link = Entry(text="Enter Link", width=50, textvariable=self.link_str, font=("Helvetica", 22, "bold"))
-        link.place(width=400, x=190, y=118)
-
-        path_lbl = Label(self, text="Enter Download Path: ", font=("Helvetica", 22, "bold"))
-        path_lbl.place(width=235, x=10, y=170)
-
-        path = Entry(text="Enter Link", width=50, textvariable=self.path_str, font=("Helvetica", 22, "bold"))
-        path.place(width=345, x=245, y=168)
-
-class MainLogic:
-    def __init__(self):
-        gui = Downloader
-
-
 
 if __name__ == "__main__":
     app = Downloader()
